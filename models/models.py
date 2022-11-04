89067c79f899a57cd6ff94c526173d8c849dc276
Tim Dang <83919404+TimXdang@users.noreply.github.com>
2022-11-04 14:22:05 +0100

    Create SOTA model

---
+++ models/models.py
@@ -0,0 +1,12 @@
+import torch
+import segmentation_models_pytorch as smp
+
+
+class UNet2DRemake:
+    def __init__(self):
+        self.encoder = 'resnext101_32x8d'
+        self.encoder_weights = 'imagenet'
+        self.activation = 'sigmoid'
+
+        self.model = smp.Unet(encoder_name=self.encoder, encoder_weights=self.encoder_weights, classes=1,
+                              activation=self.activation, in_channels=1)
