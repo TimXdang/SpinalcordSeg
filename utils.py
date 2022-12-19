637494fc535a85e4a48810f0a908251b15b5b455
Tim Dang <83919404+TimXdang@users.noreply.github.com>
2022-12-19 22:48:22 +0100

    early stop

--- utils.py
+++ utils.py
@@ -134,3 +134,18 @@ class ValidEpoch(Epoch):
                 prediction = self.model.forward(x)
             loss = self.loss(prediction, y)
         return loss, prediction
+
+
+class EarlyStopping:
+    def __init__(self, patience=5, min_delta=0):
+
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.early_stop = False
+
+    def __call__(self, new_loss, old_loss):
+        if (new_loss - old_loss) > self.min_delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
